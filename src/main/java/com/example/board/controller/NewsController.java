a111eed28f15916e8b6712a8bfdd34a224ce073a
ITYRANNOSAURUS <po04134@naver.com>
2023-10-20 12:26:19 +0900

    11

--- src/main/java/com/example/board/controller/NewsController.java
+++ src/main/java/com/example/board/controller/NewsController.java
@@ -35,7 +35,9 @@ public class NewsController {
             throw new RuntimeException("검색어 인코딩 실패", e);
         }
 
-        String apiURL = "https://openapi.naver.com/v1/search/blog?query=" + text + "&display=30";
+        String apiURL = "https://openapi.naver.com/v1/search/news?query=" + text + "&display=3";
+        // String apiURL = "https://openapi.naver.com/v1/search/blog?query=" + text +
+        // "&display=30";
         // String apiURL = "https://openapi.naver.com/v1/search/blog.xml?query="+ text;
         // // XML 결과
 
