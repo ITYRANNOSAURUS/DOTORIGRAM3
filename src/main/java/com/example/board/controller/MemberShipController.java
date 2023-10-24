b437aaba0919600693bf94f60ef983dd19391b0c
cowhub1 <sowon655@gmail.com>
2023-10-24 16:58:11 +0900

    소원 멤버십

--- src/main/java/com/example/board/controller/MemberShipController.java
+++ src/main/java/com/example/board/controller/MemberShipController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -18,40 +19,39 @@ import com.example.board.repository.UserRepository;
 
 @Controller
 public class MemberShipController {
-  @Autowired
+	@Autowired
 	UserRepository userRepository;
-	
-  @Autowired
+
+	@Autowired
 	MembershipRepository membershipRepository;
-	
+
 	@Autowired
 	HttpSession session;
 
-  @GetMapping("/membership")
+	@GetMapping("/membership")
 	public String membership() {
-		
+
 		return "membership/membership";
 	}
 
 	@PostMapping("/membership")
 	public ResponseEntity<?> membershipPost(HttpSession session) {
 		// 1. Get the user from session
-    User user = (User) session.getAttribute("user_info");
+		User user = (User) session.getAttribute("user_info");
 
 		if (user == null) {
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("User not logged in");
-    }
+			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("User not logged in");
+		}
 
 		// 2. Create a new membership
-    Membership newMembership = new Membership();
-    newMembership.setStartDate(LocalDate.now());
-    newMembership.setEndDate(LocalDate.now().plusMonths(1));
+		Membership newMembership = new Membership();
+		newMembership.setStartDate(LocalDate.now());
+		newMembership.setEndDate(LocalDate.now().plusMonths(1));
 
-		
-		//3. session user정보 newmembership에 저장
-    newMembership.setUser(user);
+		// 3. session user정보 newmembership에 저장
+		newMembership.setUser(user);
 
-		//4. newmembership정보 membershipRepository저장
+		// 4. newmembership정보 membershipRepository저장
 		membershipRepository.save(newMembership);
 
 		return ResponseEntity.ok().build();
@@ -59,13 +59,16 @@ public class MemberShipController {
 
 	@GetMapping("/subscribe")
 	public String membership2() {
-		
+
 		return "membership/subscribe";
 	}
-  
+
 	@GetMapping("/mymembership")
-	public String mymembership() {
-		
+	public String mymembership(Model model) {
+		User user = (User) session.getAttribute("user");
+		if (user != null) {
+			model.addAttribute("user", user);
+		}
 		return "membership/mymembership";
 	}
 }
