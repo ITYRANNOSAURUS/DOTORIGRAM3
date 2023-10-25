1905169dfc47ea3c8a6d4d0b125b2be8ec35fe12
sh215 <cccsue@naver.com>
2023-10-25 19:05:54 +0900

    수현수정

--- src/main/java/com/example/board/controller/ContactController.java
+++ src/main/java/com/example/board/controller/ContactController.java
@@ -14,14 +14,14 @@ public class ContactController {
 		return "contact/contact";
 	}
 
-	@PostMapping("/contact")
-	public String contact(String email, String subject, String content) {
-		Mailer mailer = new Mailer();
-		mailer.sendMail(
-			"seorab@naver.com", // 수신 이메일(관리자)
-			"[" + email + "]" + subject, // [작성자 이메일]제목
-			content, // 본문
-			new SMTPAuthenticator()); // 인증
-		return "redirect:/contact";
-	}
+// 	@PostMapping("/contact")
+// 	public String contact(String email, String subject, String content) {
+// 		Mailer mailer = new Mailer();
+// 		mailer.sendMail(
+// 			"cccsue@naver.com", // 수신 이메일(관리자)
+// 			"[" + email + "]" + subject, // [작성자 이메일]제목
+// 			content, // 본문
+// 			new SMTPAuthenticator()); // 인증
+// 		return "redirect:/contact";
+// 	}
 }
